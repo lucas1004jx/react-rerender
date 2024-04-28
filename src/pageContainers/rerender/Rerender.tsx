e69f4f765c159e6ff5a8639516340b904f853671
xin jin <lucas1004jx@gmail.com>
2024-04-28 17:23:10 +0200

    add list item

--- src/pageContainers/rerender/Rerender.tsx
+++ src/pageContainers/rerender/Rerender.tsx
@@ -1,14 +1,18 @@
 import { ComponentA } from "@/components/rerender/ComponentA";
 import { ExampleA } from "@/sectionContainer/rerender/ExampleA";
-import { Button, Typography } from "@mui/material";
+import { Button, List, Typography } from "@mui/material";
 import Link from "next/link";
 
 export const Rerennder = () => {
     return (
         <div>
         <Typography variant="h4">
-         Explore how react rerender works
+         Explore how react rerender works:
         </Typography>
+        <List>
+            <li>What is a React Component, and what is a React Element</li>
+            <li>How React rerender works</li>
+        </List>
         <Button>
             <Link href='/rerender/exampleA'>Example A</Link>
         </Button>
